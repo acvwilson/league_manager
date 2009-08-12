b662771f27b19eca54cc4e8625a7335b51092ee8
Asa Wilson <acvwilson@gmail.com>
2009-08-11 20:40:16 -0400

    Added: compass, blueprint and hamlification

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Admin::UsersController do
+
+  it "should use Admin::UsersController" do
+    controller.should be_an_instance_of(Admin::UsersController)
+  end
+
+end
