afe54e8a8366c2d86ebc589ba7b951f2588f4b94
Asa Wilson <acvwilson@gmail.com>
2009-08-25 18:01:36 -0400

    Added: a basic menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def current_class?(controller)
+    'current' if params[:controller] =~ /#{controller}/
+  end
 end
